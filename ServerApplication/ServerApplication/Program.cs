79e5b899761a18ecd9e9686509445a7b8cd5f52c
David <dyangcht@aim.com>
2021-02-07 20:47:47 +0800

    update server

--- ServerApplication/ServerApplication/Program.cs
+++ ServerApplication/ServerApplication/Program.cs
@@ -6,6 +6,9 @@ using System.Runtime.Remoting.Channels.Http;
 using ProxyLibrary;
 using System.Threading;
 
+// MSSQL
+using System.Data.SqlClient;
+
 namespace ServerApplication
 {
     class Server
@@ -13,6 +16,8 @@ namespace ServerApplication
         // private static readonly AutoResetEvent autoRestEvent = new AutoResetEvent(false);
         // private static readonly Service _service = new Service();
 
+        private SqlConnection myConnection = null;
+        private SqlDataReader myReader;
         public static void Main()
         {
             // _service.OnMessageRecieved = OnMessage_Received;
@@ -20,6 +25,47 @@ namespace ServerApplication
             HttpChannel http = new HttpChannel(4545);
             ChannelServices.RegisterChannel(http, false);
             RemotingConfiguration.RegisterWellKnownServiceType(typeof(Proxy), "pass123", WellKnownObjectMode.Singleton);
+
+            // Connect to MSSQL
+            String userid = "sa";
+            String password = "aA1TSgofwYA";
+            String server = "mssql.mssqldemo.svc.cluster.local";
+            String sqlCmd = "select Id, Name from Customers";
+            try {
+                string myConnectString = "user id=" + userid + ";password=" + password + ";Database=myContacts;Server=" + server + ";Connect Timeout=30";
+                myConnection = new SqlConnection(myConnectString);
+                myConnection.Open();
+                if (myConnection == null) {  
+                    Console.WriteLine("OPEN NULL VALUE =====================");  
+                    return;  
+                }
+                Console.WriteLine("EXECUTING .. " + sqlCmd);
+                SqlCommand myCmd = SqlCommand(sqlCmd);
+                if (myCmd == null) {
+                    Console.WriteLine("NULL VALUE...");
+                } else {
+                    myCmd.Connection = myConnection;
+                    myCmd.ExecuteNonQuery();
+                    myReader = myCmd.ExecuteReader();
+                    if (!myReader.Read()) {  
+                        myReader.Close();  
+                        return "";  
+                    }
+                    int nCol = myReader.FieldCount;
+                    string outstr = "";
+                    object[] values = new Object[nCol];
+                    myReader.GetValues(values);
+                    for (int i = 0; i < values.Length; i++) {
+                        string coldata = values[i].ToString();
+                        coldata = coldata.TrimEnd();
+                        outstr += coldata + ",";
+                    }  
+                    return outstr;
+                }
+            } catch (Exception es) {  
+                Console.WriteLine("[Error WITH DB CONNECT...] " + es.Message);  
+            }
+
             Console.WriteLine("Server is activate");
             Console.Read();
             Thread.Sleep(Timeout.Infinite);
